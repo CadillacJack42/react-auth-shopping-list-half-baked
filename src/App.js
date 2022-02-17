896e068ebdd8a853922cafd33243543804f69025
James Keicher <jamesdkeicher@gmail.com>
2022-02-16 17:22:33 -0800

    feat: set up env. set up App routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import { useState, useEffect } from 'react';
 import { getUser } from './services/fetch-utils';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import AuthPage from './AuthPage';
 import { logout } from './services/fetch-utils';
 import ListPage from './ListPage';
@@ -14,31 +9,41 @@ import './App.css';
 
 export default function App() {
   // track the user in state
+  const [user, setUser] = useState('');
 
   // add a useEffect to get the user and inject the user object into state on load
+  useEffect(() => {
+    const isUser = getUser();
+    setUser(isUser);
+  }, []);
 
   async function handleLogout() {
     // call the logout function
+    logout();
     // clear the user in state
+    setUser('');
   }
 
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <header>
           {/* if there's a user, render a logout button here */}
+          {user ? <button>Logout</button> : null}
         </header>
         <main>
           <Switch>
             <Route exact path="/">
               {/* if there is a user, redirect to the list. Otherwise, render the auth page. Note that the AuthPage will need a function called setUser that can set the user state in App.js */}
+              {user ? <AuthPage setUser={setUser} /> : <ListPage />}
             </Route>
             <Route exact path="/shopping-list">
               {/* if there's a user, take them to the list page. Otherwise, redirect them to the home/auth page */}
-
+              {user ? <ListPage /> : <AuthPage setUser={setUser} />}
             </Route>
           </Switch>
         </main>
       </div>
     </Router>
-  );}
+  );
+}
